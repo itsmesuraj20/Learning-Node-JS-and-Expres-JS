28d1f44fae1b6252ca1e101e2b7e952fc2461414
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2024-05-14 14:45:34 +0530

    Node JS Day-2 Piyush Garg Learning URL

--- HTTP/index.js
+++ HTTP/index.js
@@ -1,6 +1,7 @@
 //Built in Package used to create a server
 const http = require("http");
 const fs = require("fs");
+const url = require("url")
 
 // const myServer = http.createServer((req,res) =>{
 //     console.log("New Request Received .");
@@ -8,9 +9,25 @@ const fs = require("fs");
 // });
 
 const myServer = http.createServer((req,res) =>{
-    const log = `New Req Received at : ${Date.now()} \n`;
+    if(req.url === "/favicon.ico") return req.end;
+
+    const log = `New Req Received at : ${Date.now()} in ${req.url} \n`;
+    const myUrl = url.parse(req.url ,true);
+    
+    console.log(myUrl);
     fs.appendFile("log.txt" , log,(err,data) =>{
-        res.end("Server is working !");
+        switch(myUrl.pathname){
+            case '/': res.end("Home Page Server working OKK !");
+            break;
+
+            case '/about': 
+            const username = myUrl.query.myname;
+            res.end("About Page Server Working OKKK", `${username}`);
+            break;
+
+            default : res.end("404 ERROR");
+        }
+       
     });
     // console.log(req); // also we can log(req.headers);
     // res.end("Server is working !"); //this will be response of the particular request
